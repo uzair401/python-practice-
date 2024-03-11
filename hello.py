716d2a769e7b768550b6e871fa5c9b03249f8117
uzairsahil143 <64142661+uzairsahil143@users.noreply.github.com>
2024-03-11 16:01:39 +0500

    Add files via upload
    Sequencing DNA using Python

---
+++ hello.py
@@ -0,0 +1,14 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+
+def get_height():
+    while True:
+        n = int(input("Height: "))
+        if n > 0:
+            break
+    return n
+
+main()
